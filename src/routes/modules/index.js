f18a4b11b98318b66deea55bc4723174131247ed
“helti” <helti@qq.com>
2023-04-02 22:46:27 +0800

    feat: 文章新增与更新

--- src/routes/modules/index.js
+++ src/routes/modules/index.js
@@ -6,6 +6,8 @@ import ArticleList from "@/views/article/list";
 import NotFound from "@/views/error-pages/404";
 const Home = lazy(() => import("@/views/home"));
 const Article = lazy(() => import("@/views/article"));
+const Tags = lazy(() => import("@/views/article/tags"));
+const AddArtile = lazy(() => import("@/views/article/add"));
 
 export const routes = [
   {
@@ -37,6 +39,24 @@ export const routes = [
           roles: ["admin"],
           title: "文章列表",
           key: "/article/list",
+        },
+      },
+      {
+        path: "/article/tags",
+        element: <Tags />,
+        meta: {
+          roles: ["admin"],
+          title: "文章标签",
+          key: "/article/tags",
+        },
+      },
+      {
+        path: "/article/add",
+        element: <AddArtile />,
+        meta: {
+          roles: ["admin"],
+          title: "添加文章",
+          key: "/article/add",
         },
       },
     ],
